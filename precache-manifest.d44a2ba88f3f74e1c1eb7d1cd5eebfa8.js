be71a4fa03df327c357266c86e23892975657d73
Nmancer <datoantadze@gmail.com>
2019-02-13 10:32:56 +0400

    Updates

---
+++ precache-manifest.d44a2ba88f3f74e1c1eb7d1cd5eebfa8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/Weather-app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/Weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/Weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/Weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/Weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "ee457148f4c194dd0d10",
+    "url": "/Weather-app/static/js/runtime~main.ee457148.js"
+  },
+  {
+    "revision": "4b067482dbf2b1907768",
+    "url": "/Weather-app/static/js/main.4b067482.chunk.js"
+  },
+  {
+    "revision": "9647874d64cf0e081e0c",
+    "url": "/Weather-app/static/js/2.9647874d.chunk.js"
+  },
+  {
+    "revision": "4b067482dbf2b1907768",
+    "url": "/Weather-app/static/css/main.d27b8d6f.chunk.css"
+  },
+  {
+    "revision": "c8b71fafc712eaf34d21c6cfff33c183",
+    "url": "/Weather-app/index.html"
+  }
+];
